5b0199ac75866753379b74d82f14296918c0fe4f
mimakuma <mimo.landsberg@gmail.com>
2024-10-11 21:14:38 +0200

    Turrets.cs
    Started work on the turret script + made a basic turret prefab (+ turret sorting layer and enemy layer)

---
+++ Assets/Scripts/Turrets.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class Turrets : MonoBehaviour
+{
+    [Header("Attribute")]
+    [SerializeField] private float targetingRange = 3.5f;
+    [SerializeField] private LayerMask enemyMask;
+
+    private Transform target;
+
+    private void Update()
+    {
+        if (target == null)
+        {
+            FindTarget();
+            return;
+        }
+
+        if (!CheckTargetIsInRange())
+        {
+            target = null;
+        }
+    }
+
+    private void FindTarget()
+    {
+        RaycastHit2D[] hits = Physics2D.CircleCastAll(transform.position, targetingRange, (Vector2)transform.position, 0f, enemyMask);
+
+        if (hits.Length > 0)
+        {
+            target = hits[0].transform;
+        }
+    }
+
+    private bool CheckTargetIsInRange()
+    {
+        return Vector2.Distance(target.position, transform.position) <= targetingRange;
+    }
+    private void OnDrawGizmosSelected()
+    {
+        Handles.color = Color.green;
+        Handles.DrawWireDisc(transform.position, transform.forward, targetingRange);
+    }
+}
